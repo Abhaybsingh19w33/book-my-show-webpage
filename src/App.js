7191047d281c4aa850a73c4bcc09cba78f690bb0
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-08-05 12:16:15 +0530

    added entertainment card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Fragment } from 'react';
 import { Route } from "react-router-dom";
 
+// Import css files
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 // HOC
 import DefaultHOC from "./HOC/Default.HOC";
 
-// Components
-import Temp from "./components/temp";
+// Pages
+import HomePage from "./pages/Home.page";
 
 const App = () => {
   // function App() {
   return (
     <Fragment>
-      <DefaultHOC path="/" exact component={Temp} />
+      <DefaultHOC path="/" exact component={HomePage} />
     </Fragment>
   );
 }
