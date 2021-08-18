eb54bc3aff501cb5e3af88acd080ea92350bb3a8
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-08-18 21:48:59 +0530

    minor changes in posterSlider component for  moviePage

--- src/components/PosterSlider/PosterSlider.component.js
+++ src/components/PosterSlider/PosterSlider.component.js
@@ -9,13 +9,14 @@ import PosterCarouselSettings from "../../config/PosterCarousel.config";
 import PremierImages from "../../config/TempPosters.config";
 
 const PosterSlider = (props) => {
+    const sliderConfig = props.config ? props.config : PosterCarouselSettings;
     return (
         <Fragment>
             <div className="flex flex-col items-start">
                 <h3 className={`text-2xl font-bold ${props.isDark ? "text-white" : "text-gray-800"}`}>{props.title}</h3>
                 <p className={`text-sm ${props.isDark ? "text-white" : "text-gray-800"}`}>{props.subtitle}</p>
             </div>
-            <Slider {...PosterCarouselSettings}>
+            <Slider {...sliderConfig}>
                 {props.images.map((image) => (
                     <Poster {...image} isDark={props.isDark} />
                 ))}
