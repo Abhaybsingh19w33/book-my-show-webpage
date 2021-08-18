8bb7c1930e8c131f7b50ed65db0d27ff00e41549
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-08-18 18:17:37 +0530

    added MovieHero carousel to Moviepage

--- src/pages/Movie.page.js
+++ src/pages/Movie.page.js
@@ -1,18 +1,12 @@
 import React, { Fragment } from 'react';
 
 // Components
-import EntertainmentCardSlider from "../components/Entertainment/Entertainmentcard.component";
-import PosterSlider from '../components/PosterSlider/PosterSlider.component';
-
-// config 
-import TempPosters from "../config/TempPosters.config";
+import MovieHero from '../components/MovieHero/MovieHero.component';
 
 const Movie = () => {
     return (
         <Fragment>
-            <div>
-                <img src="https://in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/demon-slayer-mugen-train-et00313314-30-07-2021-05-14-22.jpg"/>
-            </div>
+            <MovieHero />
         </Fragment>
     );
 }
