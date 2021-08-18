72fcd6742135d9d8c6c9e9eee5a9c33bb61456b0
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-08-18 21:48:27 +0530

    added cast component for movie page

---
+++ src/components/Cast/Cast.component.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+
+const Cast = (props) => {
+    return (
+        <Fragment>
+            <div className="flex flex-col items-center">
+                <div className="w-32 h-32">
+                    <img
+                        src={props.image}
+                        alt="crew man"
+                        className="w-full h-full rounded-full"
+                    />
+                </div>
+                <h1 className="text-xl text-gray-800">{props.castName}</h1>
+                <h5 className="text-sm text-gray-500">as {props.role}</h5>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Cast;
