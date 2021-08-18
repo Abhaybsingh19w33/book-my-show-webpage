7432c4915ab3432e673d08ba6ea0b1f16a0cb612
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-08-18 13:02:30 +0530

    minor changes in poster

--- src/components/Poster/poster.component.js
+++ src/components/Poster/poster.component.js
@@ -6,13 +6,13 @@ const Poster = (props) => {
             {/* flex col to make image title and subtitle in same column */}
             {/* item start to align the item to left */}
 
-            <div className="flex flex-col items-start gap-2 px-3">
+            <div className="flex flex-col items-start gap-2 px-1 md:px-3 ">
                 {/* width is not mentioned here it will be taken care of by the carousel */}
-                <div className="h-80">
+                <div className="h-40 md:h-80">
                     <img
                         src={props.src}
                         alt={props.title}
-                        className="w-full h-full rounded-xl"
+                        className="w-full h-full rounded-md"
                     />
                 </div>
                 <h3 className={`text-lg font-bold ${props.isDark ? "text-white" : "text-gray-700"}`}>{props.title}</h3>
