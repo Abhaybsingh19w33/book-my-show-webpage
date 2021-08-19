2e9df841e636524299856ad23bced95022dbdb86
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-08-19 13:46:59 +0530

    added Link to poster.component

--- src/components/Poster/poster.component.js
+++ src/components/Poster/poster.component.js
@@ -1,23 +1,26 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 const Poster = (props) => {
     return (
         <Fragment>
-            {/* flex col to make image title and subtitle in same column */}
-            {/* item start to align the item to left */}
+            <Link to={`/movie/${props.id}`}>
+                {/* flex col to make image title and subtitle in same column */}
+                {/* item start to align the item to left */}
 
-            <div className="flex flex-col items-start gap-2 px-1 md: px-3 ">
-                {/* width is not mentioned here it will be taken care of by the carousel */}
-                <div className="h-40 md: h-80">
-                    <img
-                        src={props.src}
-                        alt={props.title}
-                        className="w-full h-full rounded-md"
-                    />
+                <div className="flex flex-col items-start gap-2 px-1 md: px-3 ">
+                    {/* width is not mentioned here it will be taken care of by the carousel */}
+                    <div className="h-40 md: h-80">
+                        <img
+                            src={`https://image.tmdb.org/t/p/original${props.poster_path}`}
+                            alt={props.original_title}
+                            className="w-full h-full rounded-md"
+                        />
+                    </div>
+                    <h3 className={`text-lg font-bold ${props.isDark ? "text-white" : "text-gray-700"}`}>{props.original_title}</h3>
+                    <p className={`text-sm ${props.isDark ? "text-white" : "text-gray-700"}`}>{props.subtitle}</p>
                 </div>
-                <h3 className={`text-lg font-bold ${props.isDark ? "text-white" : "text-gray-700"}`}>{props.title}</h3>
-                <p className={`text-sm ${props.isDark ? "text-white" : "text-gray-700"}`}>{props.subtitle}</p>
-            </div>
+            </Link>
         </Fragment>
     );
 };
