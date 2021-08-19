fa9b6c0863ab90f37b71b15ff6c805d257638507
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-08-19 13:40:54 +0530

    added axios to Appjs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { Route } from "react-router-dom";
+import axios from 'axios';
 
 // Import css files
 import 'slick-carousel/slick/slick.css';
@@ -13,6 +14,11 @@ import MovieHOC from './HOC/Movie.HOC';
 import HomePage from "./pages/Home.page";
 import Movie from './pages/Movie.page';
 import Plays from './pages/Play.page';
+
+// axois default settings
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 const App = () => {
   // function App() {
